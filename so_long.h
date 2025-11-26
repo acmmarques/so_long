ac3950a055721fa487876a93507b72eb514cdfea
André Marques <andremarques94@gmail.com>
2025-11-26 23:26:51 +0000

    added extra function & main game struct to header file

--- so_long.h
+++ so_long.h
@@ -1,7 +1,29 @@
 #ifndef SO_LONG
 # define SO_LONG
 
-# include "ft_printf.h"
 # include <fcntl.h>
+# include "ft_printf.h"
+# include "libft.h"
+
+typedef struct s_game
+{
+  void  *mlx_ptr;
+  void  *win_ptr;
+  char  **map;
+  int   height;
+  int   width;
+  int   player_x;
+  int   player_y;
+  int   collectibles_count;
+  int   player_moves;
+  int   exit_reached;
+} t_game;
+
+int   validate_input(int argc, char *filename);
+int   parse_map(char *filename, t_game *game);
+void  print_error(char *message);
+char  *ft_strjoin_free(char *s1, char *s2);
+void  free_map(t_game *game);
+int   validate_map(t_game *game);
 
 #endif
