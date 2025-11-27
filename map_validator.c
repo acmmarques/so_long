21b34577cafe08c97535cfa63c53ef80d01baa80
André Marques <andremarques94@gmail.com>
2025-11-27 01:42:15 +0000

    created map_validator file

---
+++ map_validator.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_validator.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andcardo <andcardo@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/26 19:58:56 by andcardo          #+#    #+#             */
+/*   Updated: 2025/11/27 01:25:56 by andcardo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+static int validate_map_structure(t_game *game)
+{
+  int i;
+  if (game->height == 0)
+    return (0);
+  i = 0;
+  while (i < game->height)
+  {
+    if ((int)ft_strlen(game->map[i]) != game-> width)
+      return (0);
+    i++;
+  }
+  i = 0;
+  while (i < game->width)
+  {
+    if (game->map[0][i] != '1' || game->map[game->height - 1][i] != '1') 
+      return (0);
+    i++;
+  }
+  i = 0;
+  while (i < game->height)
+  {
+    if (game->map[i][0] != '1' || game->map[i][game->width - 1] != '1') 
+      return (0);
+    i++;
+  }
+  return (1);
+}
+
+static int  count_map_elements(t_game *game)
+{
+  int i;
+  int j;
+
+  i = 0;
+  while (i < game->height)
+  {
+    j = 0;
+    while (j < game->width)
+    {
+      if (game->map[i][j] == 'C')
+        game->collectibles_count++;
+      else if (game->map[i][j] == 'P')
+      {
+        game->players_count++;
+        game->player_y = i;
+        game->player_x = j;
+      }
+      else if (game->map[i][j] == 'E')
+        game->exits_count++;
+      else if (game->map[i][j] != '1' && game->map[i][j] != '0')
+        return (0);
+      j++;
+    }
+    i++;
+  }
+  return (1);
+}
+
+static int validate_map_content(t_game *game)
+{
+  if (!count_map_elements)
+    return(0);
+  if (game->player_count > 1 ||
+      game->exits_count > 1 ||
+      game->collectibles_count < 1)
+    return (0);
+  return (1);
+}
+
+static int validate_map_path(t_game *game)
+{
+  return (1);
+}
+
+int validate_map(t_game *game)
+{
+  if (!validate_map_structure(game))
+  {
+    print_error("The map is not valid. It doesn't have the proper structure");
+    return (0);
+  }
+  if (!validate_map_content(game))
+  {
+    print_error("The map is not valid. There was a problem with it's content");
+    return (0);
+  }
+  if (!validate_map_path(game))
+  {
+    print_error("The map is not valid. Could not find a solvable path");
+    return (0);
+  }
+  return (1);
+}
